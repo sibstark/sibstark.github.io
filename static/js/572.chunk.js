49b5fb340532b1b14138b02c66100494f6b30e89
Oleg <oleg.bryuhanov@hotmail.com>
2021-04-26 14:42:38 +0700

    styled fix

--- static/js/572.chunk.js
+++ static/js/572.chunk.js
@@ -1,1 +1,1 @@
-(this["webpackJsonp@mzt/embedded-plugin"]=this["webpackJsonp@mzt/embedded-plugin"]||[]).push([[572],{1198:function(e,t,d){"use strict";d.r(t);var p=d(185);t.default=p.default}}]);
+(this["webpackJsonp@mzt/embedded-plugin"]=this["webpackJsonp@mzt/embedded-plugin"]||[]).push([[572],{1198:function(e,t,d){"use strict";d.r(t);var p=d(184);t.default=p.default}}]);
